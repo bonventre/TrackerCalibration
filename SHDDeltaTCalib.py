887479227c364499c3564c14719facc8cbb04d4b
Richard Bonventre <RBonventre@lbl.gov>
2025-08-18 17:03:52 -0700

    StrawHitDiag initial calib

---
+++ SHDDeltaTCalib.py
@@ -0,0 +1,98 @@
+import awkward as ak
+import uproot
+import numpy as np
+import sys
+import argparse
+
+import dbUtility
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run calibration')
+    parser.add_argument('--output','-o',help='output filename stub (<output>.fcl, <output>.txt)',default='output')
+    parser.add_argument('--dbpurpose','-p',help='database purpose',default='')
+    parser.add_argument('--dbversion','-v',help='database version',default='')
+    parser.add_argument('--dbtext','-t',help='comma separated list of database text files',default='')
+    parser.add_argument('--source','-s',help='input SHD filename',default='')
+    parser.add_argument('--source-list','-S',help='list of input SHD filenames',default='')
+    parser.add_argument('--dtcut','-d',type=float,help='initial dt cut (ns)',default=30.0)
+    parser.add_argument('--dtrange','-r',type=float,help='dt range (ns)',default=12.0)
+    parser.add_argument('--ecut','-e',type=float,help='energy cut (MeV)',default=0.0001)
+    parser.add_argument('--plot',action='store_true',help='plot result')
+
+    args = parser.parse_args()
+    if args.dbtext:
+        args.dbtext = args.dbtext.split(",")
+    else:
+        args.dbtext = []
+
+    if args.source:
+        fns = [args.source]
+    elif args.source_list:
+        f = open(args.source_list)
+        fns = [line.strip() for line in f.readlines()]
+    else:
+        print("Error: need to provide source or source list")
+        sys.exit(1)
+
+    aa = []
+    for batch in uproot.iterate([fn + ":StrawHitDiagnostics/shdiag" for fn in fns],filter_name=["time","edep","plane","panel","straw","runid"]):
+        batch = batch[batch.edep > args.ecut]
+        aa.append(ak.copy(batch))
+    aa = ak.concatenate(aa)
+    sids = aa.plane*96*6 + aa.panel*96 + aa.straw
+    order = ak.argsort(sids)
+    aa = aa[order]
+    sids = sids[order]
+    runs = ak.run_lengths(sids)
+    aa = ak.unflatten(aa,runs)
+
+    print("Read data")
+    
+    c = dbUtility.getCalibrations(int(aa[0].runid[0]),["TrkPreampStraw"],args.dbpurpose,args.dbversion,args.dbtext)
+    print("Read calibrations")
+
+    dts = aa.time.tcal-aa.time.thv
+    dts = dts[np.abs(dts) < args.dtcut]
+    counts = ak.count(dts,axis=1)
+    means0 = np.mean(dts,axis=1)
+    dts = dts[(dts > means0-args.dtrange) & (dts < means0+args.dtrange)]
+    means1 = np.mean(dts,axis=1)
+
+    expected_counts = np.median(counts[counts != 0])
+    goodstraw = (counts > expected_counts/5) & (counts < expected_counts*3)
+    sids = aa.plane*96*6 + aa.panel*96 + aa.straw
+
+    offsets = np.zeros(36*6*96)
+    for i in range(len(goodstraw)):
+        if goodstraw[i]:
+            offsets[sids[i][0]] = means1[i]
+
+    for i in range(36*6*96):
+        c["TrkPreampStraw"]["delay_cal"][i] = "%f" % (float(c["TrkPreampStraw"]["delay_cal"][i])-offsets[i]/2.)
+        c["TrkPreampStraw"]["delay_hv"][i] = "%f" % (float(c["TrkPreampStraw"]["delay_hv"][i])+offsets[i]/2.)
+    dbUtility.writeCalibrations(c,["TrkPreampStraw"],args.output + ".txt")
+    print('Wrote %d calibrations, avg mag = %f' % (np.sum(goodstraw),np.mean(np.abs(offsets[offsets != 0]))))
+
+    if args.plot:
+        full_sids = ak.flatten(aa.plane*96*6 + aa.panel*96 + aa.straw)
+        full_dts = ak.flatten(aa.time.tcal-aa.time.thv)
+        full_offsets = offsets[full_sids]
+        corrected_dts = full_dts - full_offsets
+    
+        import ROOT
+        h0 = ROOT.TH1F("h0","h0",300,-30,30)
+        h1 = ROOT.TH1F("h1","h1",300,-30,30)
+        h0.FillN(len(full_dts),full_dts.to_numpy().astype(np.double),np.ones(len(full_dts),dtype=np.double))
+        h1.FillN(len(full_dts),corrected_dts.to_numpy().astype(np.double),np.ones(len(full_dts),dtype=np.double))
+
+        h0.GetXaxis().SetTitle("Delta t (ns)")
+        h1.SetLineColor(ROOT.kRed)
+
+        l = ROOT.TLegend(0.65,0.65,0.85,0.85)
+        l.AddEntry(h0,"Initial","l")
+        l.AddEntry(h1,"Calibrated","l")
+    
+        h0.Draw()
+        h1.Draw("same")
+        l.Draw()
+        input()
---
+++ dbUtility.py
@@ -0,0 +1,53 @@
+from io import StringIO
+import csv
+
+import DbService
+import DbTables
+
+field_names = {"TrkPreampStraw": "index,delay_hv,delay_cal,threshold_hv,threshold_cal,gain".split(","),
+            "TrkDelayPanel": "index,delay".split(","),
+            "TrkAlignPanel": "index,strawid,dx,dy,dz,rx,ry,rz".split(",")}
+
+def getCalibrations(run,table_names,purpose="",version="",text_files=[]):
+    tables = {}
+    if purpose and version:
+        dbt = DbService.DbTool()
+        dbt.init()
+        for tn in table_names:
+            try:
+                dbt.setArgs(["print-run","--purpose",purpose,"--version",version,"--table",tn,"--run","%d" % run,"--content","1"])
+                dbt.run()
+                table = dbt.getResult()
+                tables[tn] = table
+            except Exception as e:
+                print(e)
+
+    if text_files:
+        dbu = DbTables.DbUtil()
+        for fn in text_files:
+            t = dbu.readFile(fn)
+            for i in range(len(t)):
+                if t[i].table().name() in table_names and t[i].iov().inInterval(run,1):
+                    table = t[i].table().csv()
+                    tables[t[i].table().name()] = table
+    tables = {tn: list(csv.reader(StringIO(tables[tn]))) for tn in tables}
+    results = {}
+    for tn in tables:
+        results[tn] = {field: [row[i] for row in tables[tn]] for i, field in enumerate(field_names[tn])}
+    return results
+
+def writeCalibrations(calib,table_names,filename):
+    fout = open(filename,"w")
+    for tn in table_names:
+        fout.write("TABLE %s\n" % tn)
+        for i in range(len(calib[tn][field_names[tn][0]])):
+            line = ""
+            for j in range(len(field_names[tn])):
+                line += calib[tn][field_names[tn][j]][i]
+                if j != len(field_names[tn])-1:
+                    line += ","
+            line += "\n"
+            fout.write(line)
+    fout.close()
+
+
